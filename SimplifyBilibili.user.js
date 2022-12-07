69abd78945a39fb66af20f4463f0c9a871f83b93
KeyuanHuang <60381489+KeyuanHuang@users.noreply.github.com>
2022-12-07 11:57:15 +0800

    Create SimplifyBilibili.user.js

---
+++ SimplifyBilibili.user.js
@@ -0,0 +1,53 @@
+// ==UserScript==
+// @name         Bilibili界面精简
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Bilibili界面精简
+// @author       Keyuan Huang
+// @match        *://*.bilibili.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=bilibili.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const globalBlockList = [
+        ".mini-header__title",
+        ".default-entry", //顶栏左侧元素
+        ".recommend-list-v1", //侧边栏相关视频推荐
+        ".ad-floor-exp.ad-report", //视频播放界面的广告
+        ".part-undefined.report-scroll-module.report-wrap-module.pop-live-small-mode", //直播推荐
+        ".trending", //热搜
+    ];
+
+    // 动态页面屏蔽元素
+    const tBlockList = [
+        ".left",
+        ".right", //左右侧边栏
+        ".bili-dyn-up-list", //上方up主列表
+        ".bili-dyn-publishing", //动态发布输入栏
+    ];
+
+    function update() {
+        for (const i of globalBlockList) {
+            let element = document.querySelector(i);
+            if (element) {
+                element.remove()
+            }
+        }
+
+        let regex = /t.bilibili.com/g;
+        if (regex.test(location.href)) {
+            for (const i of tBlockList) {
+                let element = document.querySelector(i);
+                if (element) {
+                    element.style.display = "none";
+                }
+            }
+        }
+    };
+
+    document.addEventListener("DOMContentLoaded", update);
+    setInterval(update, 200);
+})();
